dfc4bf730ff44a6dd6a22764a1d03fb5d77d2b3c
phan quoc duong <phanduong182k1@gmail.com>
2024-01-29 12:23:31 +0700

    commonServie done

---
+++ src/Page/common/CommonStylePhoto/CommonStylePhoto.jsx
@@ -0,0 +1,20 @@
+const CommonStylePhoto = () =>{
+    return(
+        <div className=" container">
+            <div className="title_commonStylePhoto">
+                <h3>
+                  ALBUM ẢNH CƯỚI
+                </h3>
+                <span>
+                Đa số khách hàng JEJU là các cặp đôi trẻ có phong cách thẩm mỹ hiện đại
+                </span>
+            </div>
+            <div className=" list_Style">
+                <div className=" row">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+export default CommonStylePhoto
