f8837958eed478a85431a52888ca5166b6f30455
phan quoc duong <phanduong182k1@gmail.com>
2024-02-29 22:33:03 +0700

    price page

--- src/pages/price/Price.jsx
+++ src/pages/price/Price.jsx
@@ -1,7 +1,55 @@
 import React from "react";
-
+import HeadPrice from "../common/head/head.jsx"
+import MenuPrice from "../common/tavbar/Navbar.jsx"
+import FootPrice from "../common/footer/Footer.jsx"
+import Photo1 from "../../assets/img/báo_giá_chụp/studio1.jpg"
+import Photo2 from "../../assets/img/báo_giá_chụp/studio2.jpg"
+import Photo3 from "../../assets/img/báo_giá_chụp/phim_trường_1.jpg"
+import Photo4 from "../../assets/img/báo_giá_chụp/Phim_Trường_2.jpg"
+import Photo5 from "../../assets/img/báo_giá_chụp/phim_truong3.jpg"
+import Photo6 from "../../assets/img/báo_giá_chụp/anh_phóng.jpg"
+import Photo7 from "../../assets/img/báo_giá_chụp/Đại_Học_vin.jpg"
+import Photo8 from "../../assets/img/báo_giá_chụp/dich_vụ_báo_giá.jpg"
+import Photo9 from "../../assets/img/báo_giá_chụp/full_day.jpg"
+import Photo10 from "../../assets/img/báo_giá_chụp/luxury.jpg"
+import Photo11 from "../../assets/img/báo_giá_chụp/all in.jpg"
+import "../price/price.scss"
 const Price = () => {
-  return <div>Price</div>;
+  const list = [
+    Photo1,
+    Photo2,
+    Photo3,
+    Photo4,
+    Photo5,
+    Photo6,
+    Photo7,
+    Photo8,
+    Photo9,
+    Photo10,
+    Photo11,
+  ]
+  return <>
+    <HeadPrice/>
+    <MenuPrice/>
+    <div className="container">
+        <h4>BẢNG GIÁ CHỤP ẢNH CƯỚI</h4>
+        <div className="LinePrice"></div>
+        <span>SOHA luôn là đơn vị dẫn đầu xu hướng ảnh cưới ở thị trường Hà Nội với đa dạng các phong cách từ trẻ trung, tinh tế đến sang trọng, lộng lẫy,...</span>
+        <span>Đặc biệt hơn, SOHA Wedding đã nghiên cứu và đưa ra các gói dịch vụ với mức giá cực tốt, bao gồm nhiều dịch vụ trọn gói giúp cho chi phí ngày cưới của Cô dâu Chú rể không bị phát sinh thêm nhiều.</span>
+        <div className="Box_Price">
+            <div className="item_price">
+              {list.map((element , index )=>(
+                  <div className="row">
+                    <div className="col lg-6">
+                        <img className="photo_price" key={index} src={element}/>
+                    </div>
+                  </div>
+              ))}
+            </div>
+        </div>
+    </div>
+    <FootPrice/>
+  </>;
 };
 
 export default Price;
