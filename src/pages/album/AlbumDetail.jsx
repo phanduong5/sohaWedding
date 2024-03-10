0ecb12af17398bc2b62ab534a516b6201d1d3a99
kienntt <nguyentienquyet525@gmail.com>
2024-03-10 08:26:51 +0700

    update code

--- src/pages/album/AlbumDetail.jsx
+++ src/pages/album/AlbumDetail.jsx
@@ -1,20 +1,92 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocation } from "react-router";
 import Backgroud from "../common/backgroud/Backgroud";
 import CommonContext from "../common/commonContext/CommonContext";
+import "./Album.scss";
+import { imageAlbumDetail } from "../mock-data/imagesFake";
 
 const AlbumDetail = () => {
-  let params = useLocation();
-  console.log(params);
+  let location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const idAlbum = queryParams.get("id");
+  const imageAlbumList = imageAlbumDetail();
   const bgSlide = [
     {
       id: 1,
       src: "http://localhost:3000/images/introduce/about_slider_01.jpg",
     },
+    {
+      id: 2,
+      src: "http://localhost:3000/images/introduce/about_slider_02.jpg",
+    },
+    {
+      id: 3,
+      src: "http://localhost:3000/images/introduce/about_slider_03.jpg",
+    },
+    {
+      id: 4,
+      src: "http://localhost:3000/images/introduce/about_slider_02.jpg",
+    },
+    {
+      id: 5,
+      src: "http://localhost:3000/images/introduce/about_slider_01.jpg",
+    },
+    {
+      id: 6,
+      src: "http://localhost:3000/images/introduce/about_slider_03.jpg",
+    },
   ];
+
+  const [rotateX, setRotateX] = useState(0);
+  const [rotateY, setRotateY] = useState(0);
+  const [hoveredIndex, setHoveredIndex] = useState(null);
+
+  const handleMouseMove = (index, e) => {
+    const { clientX, clientY } = e;
+    const { left, top, width, height } = e.target.getBoundingClientRect();
+
+    const rotateYValue = -30 * ((clientX - left - width / 2) / width);
+    const rotateXValue = 30 * ((clientY - top - height / 2) / height);
+
+    setRotateY(rotateYValue);
+    setRotateX(rotateXValue);
+    setHoveredIndex(index);
+  };
+
+  const handleMouseLeave = () => {
+    setRotateX(0);
+    setRotateY(0);
+    setHoveredIndex(null);
+  };
+
   return (
     <div className="service album-detail">
-      <Backgroud bgSlide={bgSlide} title="ẢNH CƯỚI STUDIO" />
+      <Backgroud bgSlide={bgSlide} title={imageAlbumList[idAlbum - 1].name} />
+      {/* Backgroud component */}
+      <div className="elementor-container">
+        <div className="gallery d-flex flex-column">
+          {imageAlbumList[idAlbum - 1].list.map((image, index) => (
+            <div
+              className="image-container col-md-4"
+              key={index}
+              onMouseMove={(e) => handleMouseMove(index, e)}
+              onMouseLeave={handleMouseLeave}
+            >
+              <img
+                src={image}
+                alt={`Image ${index}`}
+                className="album-detail-img"
+                style={{
+                  transform:
+                    hoveredIndex === index
+                      ? `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale3d(1, 1, 1)`
+                      : "",
+                }}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
       <CommonContext />
     </div>
   );
---
+++ src/pages/mock-data/imagesFake.js
@@ -0,0 +1,53 @@
+const images1 = require.context(
+  "../../assets/img/Abumle_ảnh_cươi/ảnh_chụp_hồ",
+  true
+);
+const images2 = require.context(
+  "../../assets/img/Abumle_ảnh_cươi/ảnh_chụp_ngoài_trời_tối",
+  true
+);
+const images3 = require.context(
+  "../../assets/img/Abumle_ảnh_cươi/ảnh_cổ",
+  true
+);
+const images4 = require.context(
+  "../../assets/img/Abumle_ảnh_cươi/ảnh_ngay_xưa",
+  true
+);
+const images5 = require.context(
+  "../../assets/img/Abumle_ảnh_cươi/ảnh_studio_2",
+  true
+);
+const images6 = require.context(
+  "../../assets/img/Abumle_ảnh_cươi/phimtruong1",
+  true
+);
+
+export const imageAlbumDetail = () => {
+  const imageMap1 = {
+    name: "ẢNH CHỤP HỒ",
+    list: images1.keys().map((image) => images1(image)),
+  };
+  const imageMap2 = {
+    name: "ẢNH CHỤP NGOÀI TRỜI TỐI",
+    list: images2.keys().map((image) => images2(image)),
+  };
+  const imageMap3 = {
+    name: "ẢNH CỔ",
+    list: images3.keys().map((image) => images3(image)),
+  };
+  const imageMap4 = {
+    name: "ẢNH NGÀY XƯA",
+    list: images4.keys().map((image) => images4(image)),
+  };
+  const imageMap5 = {
+    name: "ẢNH STUDIO 2",
+    list: images5.keys().map((image) => images5(image)),
+  };
+  const imageMap6 = {
+    name: "ẢNH PHIM TRƯỜNG 1",
+    list: images6.keys().map((image) => images6(image)),
+  };
+
+  return [imageMap1, imageMap2, imageMap3, imageMap4, imageMap5, imageMap6];
+};
